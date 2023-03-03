3cf3de3e79504f0accf963d943f5cd0e894918dc
“NMeyer1134” <nathaniel.meyer@lethbridgecollege.ca>
2023-03-03 13:42:31 -0700

    Edit, Delete, and Success Messages

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Note;
+use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class NoteController extends Controller
 {
@@ -38,6 +40,7 @@ class NoteController extends Controller
         ]);
 
         Note::create([
+            'uuid' => Str::uuid(),
             'user_id' => Auth::id(),
             'title' => $request->title,
             'text' => $request->text
@@ -48,33 +51,62 @@ class NoteController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Note $note)
     {
-        $note = Note::where('id', $id)->where('user_id', Auth::id())->firstOrFail();
+        //$note = Note::where('uuid', $uuid)->where('user_id', Auth::id())->firstOrFail();
+
+        //read up on gates and policies so you don't have to use if statements for every function
+        if($note->user_id != Auth::id()){
+            return abort(403);
+        }
+
         return view('notes.show')->with('note', $note);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Note $note)
     {
-        //
+        if($note->user_id != Auth::id()){
+            return abort(403);
+        }
+
+        return view('notes.edit')->with('note', $note);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Note $note)
     {
-        //
+        if($note->user_id != Auth::id()){
+            return abort(403);
+        }
+
+        $request->validate([
+            'title' => 'required|max:120',
+            'text' => 'required'
+        ]);
+
+        $note->update([
+            'title' => $request->title,
+            'text' => $request->text
+        ]);
+        return to_route('notes.show', $note)->with('success', 'Note Updated Successfully!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Note $note)
     {
-        //
+        if($note->user_id != Auth::id()){
+            return abort(403);
+        }
+
+        $note->delete();
+
+        return to_route('notes.index')->with('success', 'Note Deleted Successfully!');
     }
 }
